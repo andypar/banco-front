7f3c2a93baeb31e179c4c2a60c1de406cbb99f3a
andypar <andypar@users.noreply.github.com>
2023-02-07 01:03:09 -0300

    pruebas sign in

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,23 @@
-
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import UserList from "../modules/UserList";
 import AppStatus from "../modules/AppStatus";
 import Main from "../layout/Main";
+import SignIn from "../modules/SignIn";
 
 
 function Router() {
-
   return (
-	<BrowserRouter>
-    <Main>
-		<Routes>
-			<Route exact path="/" element={<AppStatus/>} />
-			<Route exact path="/user" element={<UserList/>} />
-	  </Routes>
-    </Main>
-	</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" exact element={<SignIn />} />
+      </Routes>
+      <Main>
+        <Routes>
+          <Route exact path="/" element={<AppStatus />} />
+          <Route exact path="/user" element={<UserList />} />
+        </Routes>
+      </Main>
+    </BrowserRouter>
   );
 }
 
